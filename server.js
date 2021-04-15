d340d1ba281013f0a8d5d55d568d5cadc8353cfd
sivakami <abisivakami91@gmail.com>
2021-04-15 15:03:34 +0530

    cart page,auth

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const app =express();
 const ejs=require('ejs')
 const path = require("path");
 const expressLayout=require("express-ejs-layouts")
+app.use(express.static('public'))
 
 
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render("home")
 })
-
-app.use(expressLayout)
-app.use(express.static('public'));
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render("customer/cart")
+})
+app.get('/login', (req, res) => {
+    res.render("auth/login")
+})
+app.get('/register', (req, res) => {
+    res.render("auth/register")
+})
 app.listen(3000, () => {
     console.log("server stated");
 })
